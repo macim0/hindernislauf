6be3dd7b2d959ebcc94a6693c8da3c8a1caced77
macim0 <44290141+macim0@users.noreply.github.com>
2021-02-09 11:28:13 +0100

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,8 @@
+input.onButtonPressed(Button.A, function () {
+    Spielfigur.move(1)
+})
+let Spielfigur: game.LedSprite = null
+Spielfigur = game.createSprite(2, 4)
+basic.forever(function () {
+	
+})
