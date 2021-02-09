da1009f6a11c409ebe3f47aadfae3d9c08e4c7b0
macim0 <44290141+macim0@users.noreply.github.com>
2021-02-09 15:26:41 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,1 @@
-input.onButtonPressed(Button.A, function () {
-    Spielfigur.move(1)
-})
-let Spielfigur: game.LedSprite = null
-Spielfigur = game.createSprite(2, 4)
-basic.forever(function () {
-	
-})
+
